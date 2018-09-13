9f514d22334e9844610265bf9ade26def6a4a6b9
Enzeir <lehenaffpierre92@gmail.com>
2018-09-13 17:53:09 -0400

    added pen image, WIP added script of checking and unchecking the new participants info, does not work not sure why...

--- TD1.js
+++ TD1.js
@@ -5,3 +5,13 @@ function showPen(e) {
 function hidePen(e) {
   document.getElementById(e).style.visibility="hidden";
 }
+function clickedCheck(e) {
+
+    if (document.getElementById(e).src == "./Images/check.png") {
+        document.getElementById(e).src = "./Images/tick-check.png";
+    }
+    else{
+        document.getElementById(e).src = "./Images/check.png";
+    }
+    
+}
