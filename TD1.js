ee427f5372102391d5c77ccc7dc70b71fd6f9de5
Pierre-Erwan Le Henaff <pilee@l4714-10.info.polymtl.ca>
2018-09-26 10:10:22 -0400

    added the checkboxes

--- TD1.js
+++ TD1.js
@@ -6,8 +6,8 @@ function hidePen(e) {
   document.getElementById(e).style.visibility="hidden";
 }
 function clickedCheck(e) {
-
-    if (document.getElementById(e).src == "./Images/check.png") {
+	console.log(document.getElementById(e).src);
+	if (document.getElementById(e).src == "./Images/check.png") {
         document.getElementById(e).src = "./Images/tick-check.png";
     }
     else{
