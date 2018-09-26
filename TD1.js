a3f4b0730c75ee0244f00c185538cb53bf4abe66
Pierre-Erwan Le Henaff <pilee@l4714-10.info.polymtl.ca>
2018-09-26 11:29:01 -0400

    added script to fetch json file

--- TD1.js
+++ TD1.js
@@ -15,3 +15,9 @@ function clickedCheck(e) {
     }
     
 }
+fetch("http://localhost:8080/cal-data.json")
+  .then(function(response) { return response.json(); })
+  .then(function(data) {
+    console.log(data["Calendrier"][0]);
+    
+  });
