1e2b9b6fcda58b32aa07d8cd6e28a112868ded3d
Pierre-Erwan Le Henaff <pilee@l4714-01.info.polymtl.ca>
2018-10-04 13:15:40 -0400

    new code from last commit to a function and added code to create a new participant row

--- calendrier.js
+++ calendrier.js
@@ -1,4 +1,18 @@
 
+
+fetch("http://127.0.0.1:8080/cal-data.json")
+  .then(function(response) { return response.json(); })
+  .then(function(data) {
+
+	  var listParticipant = data["Participants"];
+	  var listDate = data["Calendrier"];
+	  
+	  createDates(listDate);
+	  createParticipants(listParticipant);
+	  
+
+  });
+
 function showPen(e) {
   document.getElementById(e).style.visibility="visible";
 }
@@ -13,64 +27,7 @@ function clickedCheck(e) {
     else{
         document.getElementById(e).src = "./Images/check.png";
     }
-    
 }
-fetch("http://127.0.0.1:8080/cal-data.json")
-  .then(function(response) { return response.json(); })
-  .then(function(data) {
-	  
-	  var participants = data["Participants"];
-
-	  for(var i = 1; i < participants.length; i++)
-		  {
-		  var Participantrow = document.createElement('div');
-		  Participantrow.setAttribute("class", "ParticipantRow");
-		  
-		  var firstBox = document.createElement('div');
-		  firstBox.setAttribute("onmouseover" ,"showPen('pen"+i+"')");
-		  firstBox.setAttribute("onmouseout" ,"hidePen('pen"+i+"')");
-		  firstBox.setAttribute("class", "firstBox");
-		  Participantrow.appendChild(firstBox);
-		  
-		  var participantimg = document.createElement('img');
-		  participantimg.setAttribute("src","./Images/particip2.png");
-		  firstBox.appendChild(participantimg);
-		  
-		  var t = document.createTextNode(data["Participants"][i]["Nom"]);
-		  firstBox.appendChild(t);
-		  
-		  var penimg = document.createElement('img');
-		  penimg.setAttribute("class", "pen");
-		  penimg.setAttribute("id", "pen"+i);
-		  penimg.setAttribute("onClick", "fsuyf('participant"+i+"')");
-		  penimg.setAttribute("src","./Images/pen.png");
-		  firstBox.appendChild(penimg);
-		  
-		  document.getElementById("1234").appendChild(Participantrow);
-		  
-		  var disp = data["Participants"][i]["Disponibilités"];
-		  
-		  for(var j = 0; j<disp.length; j++)
-			  {
-			  var choicebox = document.createElement('div');
-			  choicebox.setAttribute("class", "choiceBox");
-			  var tickImage = document.createElement('img');
-			  tickImage.setAttribute("class", "tickImage");
-			  tickImage.setAttribute("src", "./Images/tick1.png");
-
-			  if(disp[j] == 1)
-				  {
-				  var tickImage = document.createElement('img');
-				  tickImage.setAttribute("class", "tickImage");
-				  tickImage.setAttribute("src", "./Images/tick1.png");
-				  choicebox.appendChild(tickImage);
-				  } 
-			  Participantrow.appendChild(choicebox);
-		  
-			  }
-		  }
-
-  });
 
 function myFunction() {
     var btn = document.createElement("BUTTON");
@@ -78,3 +35,110 @@ function myFunction() {
     btn.appendChild(t);
     document.getElementById("1234").appendChild(btn);
 }
+
+
+function createDates(datesList)
+{
+	
+}
+
+
+
+function createParticipants(listParticipants)
+{
+	  for(var i = 0; i < listParticipants.length; i++)
+	  {
+		  if(listParticipants[i]["Statut"] != "EnCours")
+		  {
+			  var Participantrow = document.createElement('div');
+			  Participantrow.setAttribute("class", "ParticipantRow");
+			  
+			  var firstParticipantBox = document.createElement('div');
+			  firstParticipantBox.setAttribute("onmouseover" ,"showPen('pen"+i+"')");
+			  firstParticipantBox.setAttribute("onmouseout" ,"hidePen('pen"+i+"')");
+			  firstParticipantBox.setAttribute("class", "firstBox");
+			  Participantrow.appendChild(firstParticipantBox);
+			  
+			  var participantimg = document.createElement('img');
+			  participantimg.setAttribute("src","./Images/particip2.png");
+			  firstParticipantBox.appendChild(participantimg);
+			  
+			  var t = document.createTextNode(listParticipants[i]["Nom"]);
+			  firstParticipantBox.appendChild(t);
+			  
+			  var penimg = document.createElement('img');
+			  penimg.setAttribute("class", "pen");
+			  penimg.setAttribute("id", "pen"+i);
+			  penimg.setAttribute("onClick", "fsuyf('participant"+i+"')");
+			  penimg.setAttribute("src","./Images/pen.png");
+			  firstParticipantBox.appendChild(penimg);
+			  
+			  document.getElementById("1234").appendChild(Participantrow);
+			  
+			  var disp = listParticipants[i]["Disponibilités"];
+			  
+			  for(var j = 0; j<disp.length; j++)
+			  {
+				  var choicebox = document.createElement('div');
+				  choicebox.setAttribute("class", "choiceBox");
+		
+				  if(disp[j] == 1)
+				  {
+					  var tickImage = document.createElement('img');
+					  tickImage.setAttribute("class", "tickImage");
+					  tickImage.setAttribute("src", "./Images/tick1.png");
+					  choicebox.appendChild(tickImage);
+				  } 
+				  Participantrow.appendChild(choicebox);
+			  }
+		  }
+		  else
+		  {
+		  	var newParticipantRow = document.createElement('div');
+		  	newParticipantRow.setAttribute("class", "newParticipantRow");
+
+		  	
+		  	var newParticipantFirstBox = document.createElement('div');
+		  	newParticipantFirstBox.setAttribute("class", "firstBox");
+		  	newParticipantRow.appendChild(newParticipantFirstBox);
+		  	
+		  	var newParticipantImg = document.createElement('img');
+		  	newParticipantImg.setAttribute("src", "./Images/particip1.png");
+		  	newParticipantFirstBox.appendChild(newParticipantImg);
+		  	
+		  	var newParticipantName = document.createTextNode(listParticipants[i]["nom"]);
+		  	newParticipantFirstBox.appendChild(newParticipantName);
+
+		  	
+		  	
+		  	var newParticipantInput = document.createElement('input');
+		  	newParticipantInput.setAttribute("type", "text");
+		  	newParticipantInput.setAttribute("value", newParticipantName);
+		  	newParticipantFirstBox.appendChild(newParticipantInput);
+		  	
+		  	document.getElementById("1234").appendChild(newParticipantRow);
+		  	
+		  	var disp = listParticipants[i]["Disponibilités"];
+		  	
+		  	for(var j = 0; j < disp.length; j++)
+	  		{
+		  		var checkboxClass = document.createElement('div');
+		  		checkboxClass.setAttribute("class", "checkBox");
+		  		
+		  		var checkboxInput = document.createElement('input');
+		  		checkboxInput.setAttribute("type", "checkbox");
+		  		
+		  		if(disp[j] == 1)
+		  			{
+		  			checkboxInput.setAttribute("checked", true);
+		  			}
+		  		checkboxClass.appendChild(checkboxInput);
+		  		
+		  		newParticipantRow.appendChild(checkboxClass);
+	  		}
+		  
+		  }
+	  }
+}
+
+
